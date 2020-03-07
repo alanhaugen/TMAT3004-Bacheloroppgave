25eea8022716fae757e5be7fb99a71c7e98e1954
Alan <alanhaugen@gmail.com>
2020-03-07 18:18:46 +0100

    Added a proper working template for app

--- app/main.cpp
+++ app/main.cpp
@@ -5,35 +5,12 @@ using namespace std;
 using namespace matplotlibcpp;
 using namespace cv;
 
-Mat cartoonify(Mat image, int arguments=0)
-{
-    Mat cartoonImage;
-
-    /// YOUR CODE HERE
-
-    return cartoonImage;
-}
-
-Mat pencilSketch(Mat image, int arguments=0)
-{
-    Mat pencilSketchImage;
-    
-    /// YOUR CODE HERE
-
-    return pencilSketchImage;
-}
-
 int main()
 {
-    string imagePath = "images/trump.jpg";
+    string imagePath = "images/opencv-1-cpp";
     Mat image = imread(imagePath);
 
-    Mat cartoonImage = cartoonify(image);
-    Mat pencilSketchImage = pencilSketch(image);
-
-    imshow("Original Image", image);
-    //imshow("Pencil Sketch Result", cartoonImage);
-    //imshow("Cartoon Filter Result", pencilSketchImage);
+    imshow("Image", image);
 
     waitKey(0);
 }
