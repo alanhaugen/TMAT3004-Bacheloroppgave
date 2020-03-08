67fce3597c069bba0535b3eaeb9a575bb34f9a3c
Alan <alanhaugen@gmail.com>
2020-03-08 17:10:26 +0100

    Added code for object tracking, not linking win64

--- app/main.cpp
+++ app/main.cpp
@@ -1,8 +1,9 @@
 #include <opencv2/opencv.hpp>
-#include "matplotlibcpp.h"
+#include <opencv2/tracking.hpp>
+//#include "matplotlibcpp.h"
 
 using namespace std;
-using namespace matplotlibcpp;
+//using namespace matplotlibcpp;
 using namespace cv;
 
 int main()
@@ -21,6 +22,30 @@ int main()
 
     Mat frame;
 
+    string trackerTypes[8] = {"BOOSTING", "MIL", "KCF", "TLD", "MEDIANFLOW", "GOTURN", "CSRT", "MOSSE"};
+
+    string trackerType = trackerTypes[6];
+
+    Ptr<Tracker> tracker;
+    Rect2d bbox(204, 131, 97, 222);
+
+    if (trackerType == "BOOSTING")
+        tracker = TrackerBoosting::create();
+    else if (trackerType == "MIL")
+        tracker = TrackerMIL::create();
+    else if (trackerType == "KCF")
+        tracker = TrackerKCF::create();
+    else if (trackerType == "TLD")
+        tracker = TrackerTLD::create();
+    else if (trackerType == "MEDIANFLOW")
+        tracker = TrackerMedianFlow::create();
+    else if (trackerType == "GOTURN")
+        tracker = TrackerGOTURN::create();
+    else if (trackerType == "CSRT")
+        tracker = TrackerCSRT::create();
+    else if (trackerType == "MOSSE")
+        tracker = TrackerMOSSE::create();
+
     while(char c = waitKey(25) != 27)
     {
         video >> frame;
@@ -29,6 +54,19 @@ int main()
         {
             video.set(CAP_PROP_POS_FRAMES, 1);
             video >> frame;
+        }
+
+        bool ok = tracker->update(frame, bbox);
+
+        if (ok)
+        {
+            // Tracking success : Draw the tracked object
+            rectangle(frame, bbox, Scalar( 255, 0, 0 ), 2, 1 );
+        }
+        else
+        {
+            // Tracking failure detected.
+            putText(frame, "Tracking failure detected", Point(100,80), FONT_HERSHEY_SIMPLEX, 0.75, Scalar(0,0,255),2);
         }
 
         imshow("TMAT3004", frame);
